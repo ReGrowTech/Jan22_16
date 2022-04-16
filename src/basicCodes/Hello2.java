42cf40f4c3f8b3cb7ae1ee748f0aead99777b315
Regrowtech <regrowtech@gmail.com>
2022-04-16 23:20:54 +0530

    Added my name

--- src/basicCodes/Hello2.java
+++ src/basicCodes/Hello2.java
@@ -4,7 +4,7 @@ public class Hello2 {
 
 	public static void main(String[] args) {
 		System.out.println("I am Hello 2");
-		System.out.println("Hello Regrow Team");
+		System.out.println("Hello Regrow Team. I am Faizan");
 
 	}
 
