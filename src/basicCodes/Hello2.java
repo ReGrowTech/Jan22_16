1f8f10f2202347e6a70b0836d93cf4fa0b7176e3
Nadeem <nadeem.tarique9@gmail.com>
2022-04-17 16:31:20 +0530

    changed in Hello2

--- src/basicCodes/Hello2.java
+++ src/basicCodes/Hello2.java
@@ -5,6 +5,7 @@ public class Hello2 {
 	public static void main(String[] args) {
 		System.out.println("I am Hello 2. I am making a change to check conflict");
 		System.out.println("Hello Regrow Team. I am Faizan. I am teaching");
+		System.out.println("Hi all, welcome to Regrow Tech");
 
 
 	}
