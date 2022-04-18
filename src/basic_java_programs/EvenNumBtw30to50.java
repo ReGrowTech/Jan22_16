d82efebbb6986f3ed8859b0aa2a8158cf88bd89a
Prasenjit <senprasenjit1994@gmail.com>
2022-04-18 22:20:38 +0530

    All Even nos between 30 to 50 program added

---
+++ src/basic_java_programs/EvenNumBtw30to50.java
@@ -0,0 +1,12 @@
+//Capsule 8 question 4
+package basic_java_programs;
+
+public class EvenNumBtw30to50 {
+
+	public static void main(String[] args) {
+		System.out.println("All Even numbers between 30 and 50 are :");
+		for (int i =30;i<=50;i=i+2)
+			System.out.print(i+"\t");
+	}
+
+}
