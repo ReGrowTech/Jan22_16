be5295a9ffdb01e1697bb661fa31c8998b1133ec
SaniaAkhtar <saniaakhtar33@gmail.com>
2022-04-19 16:10:23 +0530

    FirstCommit

--- src/basicCodes/Firstcommit.java
+++ src/basicCodes/Firstcommit.java
@@ -3,7 +3,7 @@ package basicCodes;
 public class Firstcommit {
 
 	public static void main(String[] args) {
-		System.out.println("This is my first commit");
+		System.out.println("This is my first commit in git");
 
 	}
 
