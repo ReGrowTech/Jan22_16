87af12ed57e21c840771c997745aeb6391c72f2b
prasenjit <senprasenjit1994@gmail.com>
2022-04-20 12:40:31 +0530

    modified leap year codes

--- src/My_swapping/Leap_year.java
+++ src/My_swapping/Leap_year.java
@@ -6,17 +6,18 @@ public class Leap_year {
 
 	public static void main(String[] args) {
 
-		int year;
+	
 		Scanner sc = new Scanner(System.in);
-		 year = sc.nextInt();
+		System.out.println("Enter Year: ");
+		int  year = sc.nextInt();
 		
 		if(year%4==0)
 		{
-			System.out.println("This is a leap year"+year);
+			System.out.println(year+" is a leap year");
 		}
 		else
 		{
-			System.out.println("This is not a leap year"+year);
+			System.out.println(year+" is not a leap year");
 		}
 		
 		
