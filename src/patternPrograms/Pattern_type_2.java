6db938cb0cab11f8b3394cb39ab5aa964342d089
Prasenjit <senprasenjit1994@gmail.com>
2022-04-21 22:15:16 +0530

    added pattern prog

---
+++ src/patternPrograms/Pattern_type_2.java
@@ -0,0 +1,23 @@
+//2 
+//2 4 
+//2 4 6 
+//2 4 6 8 
+//2 4 6 8 10 
+
+package patternPrograms;
+
+public class Pattern_type_2 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(j*2+" ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
