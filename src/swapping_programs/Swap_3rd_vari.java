ce81b4c9ff4d1ac811cb2f4caa646c948bc7dc32
Prasenjit <senprasenjit1994@gmail.com>
2022-04-24 11:50:39 +0530

    added swaping program using 3rd variable

---
+++ src/swapping_programs/Swap_3rd_vari.java
@@ -0,0 +1,21 @@
+package swapping_programs;
+
+public class Swap_3rd_vari {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		System.out.println("Before swap :");
+		System.out.println("a : "+a );
+		System.out.println("b : "+b );
+		int c=0;
+		c=a;
+		a=b;
+		b=c;
+		System.out.println("After swap :");
+		System.out.println("a : "+a );
+		System.out.println("b : "+b );
+
+	}
+
+}
