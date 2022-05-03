cfa330a0ad50b621f5853575d4b95043794a51d2
prasenjit <senprasenjit1994@gmail.com>
2022-05-03 17:32:56 +0530

    practising

---
+++ src/may/Sen_program/ArraylistDescending.java
@@ -0,0 +1,30 @@
+package may.Sen_program;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
+
+public class ArraylistDescending
+{
+
+	public static void main(String[] args) 
+	{
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		al.add(10);
+		al.add(100);
+		al.add(20);
+		al.add(300);
+		al.add(35);
+		Collections.sort(al,Collections.reverseOrder());
+		java.util.Iterator itr=al.iterator();
+		while(itr.hasNext())
+		{
+			System.out.println(itr.next());
+		}
+		
+				
+
+	}
+
+}
